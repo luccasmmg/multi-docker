73a47575fa975fec07d227f8a94b88ab1673f95d
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2020-03-31 22:03:05 -0300

    Change test, to check if updates are working in deploy

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <div className='App'>
+        <h1 className='App-Title'>Fib Calculator</h1>
         <header className='App-header'>
           <Link to='/'>Home</Link>
           <Link to='/otherpage'>Other Page</Link>
